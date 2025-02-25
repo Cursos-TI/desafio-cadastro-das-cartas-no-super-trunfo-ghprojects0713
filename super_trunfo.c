b5ad4b5e37d16ac5cad5e21e46aaed8ba56d74e1
ghprojects0713 <gflestudos@gmail.com>
2025-02-25 19:45:40 +0000

    criei as cartas

---
+++ super_trunfo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    printf("Super Trunfo\n");
+    printf(" \n");
+
+
+    
+    printf("Carta 1\n");
+    printf("Estado: Minas Gerais\n");
+    printf("Código da carta: A01\n");
+    printf("Nome da cidade: Belo Horizonte\n");
+    printf("População: 2315560\n");
+    printf("Área: 331.3 km²\n");
+    printf("PIB: 49 bilhões de dólares\n");
+    printf("Pontos turísticos: 28\n");
+    printf(" \n");
+
+    printf("Carta 2\n");
+    printf("Estado: Flórida\n");
+    printf("Código da carta: B01\n");
+    printf("Nome da cidade: Orlando\n");
+    printf("População: 2509454\n");
+    printf("Área: 261.5 km²\n");
+    printf("PIB: 121 bilhões de dólares\n");
+    printf("Pontos turísticos: 100\n");
+
+}
