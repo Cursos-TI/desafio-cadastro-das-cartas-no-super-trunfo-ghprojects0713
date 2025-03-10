568d23d80b38f517c2fabf89250747a4356ca797
ghprojects0713 <gflestudos@gmail.com>
2025-03-10 18:20:06 +0000

    terminei o desafio novato

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(){
+    char estado1 [50] = "Minas Gerais";
+    char codigo1 [5] = "A01";
+    char cidade1 [50] = "Belo Horizonte";
+    int populacao1 = 2315560;
+    float area1 = 331.3;
+    float PIB1 = 49.000000000;
+    int pontosturisticos1 = 28;
+
+
     printf("Super Trunfo\n");
     printf(" \n");
 
@@ -13,6 +22,14 @@ int main(){
     printf("PIB: 49 bilhões de dólares\n");
     printf("Pontos turísticos: 28\n");
     printf(" \n");
+
+    char estado2 [50] = "Flórida";
+    char codigo2 [5] = "B01";
+    char cidade2 [50] = "Orlando";
+    int populacao2 = 2509454;
+    float area2 = 261.5;
+    float PIB2 = 121.000000000;
+    int pontosturisticos2 = 100;
 
     printf("Carta 2\n");
     printf("Estado: Flórida\n");
